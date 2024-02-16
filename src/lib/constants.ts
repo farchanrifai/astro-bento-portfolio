710853336d006046f510f58c7a8956c2e5e14007
farchanrifai <farchanrifaii@gmail.com>
2024-02-17 00:59:19 +0700

    Second Deployment

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,5 @@
 export const LINKS = {
-  github: "https://github.com/Ladvace",
+  github: "https://github.com/farchanrifai",
   linkedin: "https://www.linkedin.com/in/farchanrifai/",
   medium: "https://ladvace.medium.com/",
   discord: "https://discordapp.com/users/163300027618295808",
